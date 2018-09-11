51e47a79e9c96b2a6bb5bc17aa87869e5a6bb8c5
vitaliitsupylo <vitalii.tsupylo@gmail.com>
2018-09-12 01:44:26 +0300

    methods get data аÑfor infoline

--- src/components/infoLine.vue
+++ src/components/infoLine.vue
@@ -3,19 +3,65 @@
         <div class="container">
             <div class="weather">
                 <span class="city">Киев </span>
-                <span class="temperature"></span>&#176;C
+                <span class="temperature">{{ temperature }}</span>&#176;C
             </div>
             <div class="currency">
-                КУРС ВАЛЮТ <i class="icon-dollar"></i><span class="dol"></span><i class="icon-euro"></i><span
-                    class="euro"></span>
+                КУРС ВАЛЮТ <i class="icon-dollar"></i><span class="dol">{{currencyUSD}}</span><i
+                    class="icon-euro"></i><span
+                    class="euro">{{currencyEUR}}</span>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    const axios = require('axios');
+
+    function getValue(arr, str) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i]['ccy'].toUpperCase() === str.toUpperCase()) {
+                return arr[i]['sale'].slice(0, 5);
+            }
+        }
+    }
+
     export default {
-        name: 'infoLine'
+        name: 'infoLine',
+        data() {
+            return {
+                temperature: this.getTemperature(),
+                currencyUSD: '00',
+                currencyEUR: '00',
+                currency: this.getCurrency()
+            }
+        },
+        methods: {
+            getTemperature() {
+                axios({
+                    method: 'get',
+                    url: 'http://api.openweathermap.org/data/2.5/weather?lat=50.4546600&lon=30.5238000&APPID=eb8183acf2cb85c1351fe1826283fc36'
+                })
+                    .then((res) => {
+                        let tempData = Math.round(res.data.main.temp - 273);
+                        this.temperature = (tempData > 0) ? `+ ${tempData}` : tempData;
+                    }, () => {
+                        this.temperature = ' no Internet';
+                    });
+            },
+            getCurrency() {
+                axios({
+                    method: 'get',
+                    url: 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
+                })
+                    .then((res) => {
+                        this.currencyUSD = getValue(res.data, 'USD');
+                        this.currencyEUR = getValue(res.data, 'EUR');
+                    }, () => {
+                        this.currencyUSD = '00';
+                        this.currencyEUR = '00';
+                    });
+            }
+        }
     }
 </script>
 
